02dbf404219a85c66011af1d0f5843e0645c49e0
bill7maxmaye <bilenmehalek7@gmail.com>
2024-05-07 12:02:16 +0300

    created user detail api endpoint

--- Server/controller/checkPassword.js
+++ Server/controller/checkPassword.js
@@ -21,7 +21,7 @@ async function checkPassword(request, response) {
       id: user._id,
       email: user.email,
     };
-    const token = await jwt.sign(tokenData, process.env.JWT_SECREAT_KEY, {
+    const token = await jwt.sign(tokenData, process.env.JWT_SECRET_KEY, {
       expiresIn: "1d",
     });
 
--- Server/routes/index.js
+++ Server/routes/index.js
@@ -1,10 +1,12 @@
 const express = require("express");
 const registerUser = require("../controller/registeruser");
 const checkEmail = require("../controller/checkEmail");
+const checkPassword = require("../controller/checkPassword");
 const router = express.Router();
 
 //create the route to be used to register a user
 router.post("/register", registerUser);
 router.post("/email", checkEmail);
+router.post("/password", checkPassword);
 
 module.exports = router;
