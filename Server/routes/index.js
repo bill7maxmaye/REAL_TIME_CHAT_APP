d81180abbbc249acae209eacb8c5389410c3ee6b
bill7maxmaye <bilenmehalek7@gmail.com>
2024-05-07 12:14:16 +0300

    created user detail

--- Server/routes/index.js
+++ Server/routes/index.js
@@ -2,11 +2,16 @@ const express = require("express");
 const registerUser = require("../controller/registeruser");
 const checkEmail = require("../controller/checkEmail");
 const checkPassword = require("../controller/checkPassword");
+const userDetails = require("../controller/userDetails");
 const router = express.Router();
 
 //create the route to be used to register a user
 router.post("/register", registerUser);
+//check user email
 router.post("/email", checkEmail);
+//check user password
 router.post("/password", checkPassword);
+//log in user details
+router.get("/user-details", userDetails);
 
 module.exports = router;
