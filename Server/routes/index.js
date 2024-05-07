f61ca48cf41013c6e91756d3d7e909fb4c3a3494
bill7maxmaye <bilenmehalek7@gmail.com>
2024-05-07 12:30:41 +0300

    created logout endpoint

--- Server/routes/index.js
+++ Server/routes/index.js
@@ -3,6 +3,7 @@ const registerUser = require("../controller/registeruser");
 const checkEmail = require("../controller/checkEmail");
 const checkPassword = require("../controller/checkPassword");
 const userDetails = require("../controller/userDetails");
+const logout = require("../controller/logout");
 const router = express.Router();
 
 //create the route to be used to register a user
@@ -13,5 +14,7 @@ router.post("/email", checkEmail);
 router.post("/password", checkPassword);
 //log in user details
 router.get("/user-details", userDetails);
+//logout user
+app.get("logout", logout);
 
 module.exports = router;
