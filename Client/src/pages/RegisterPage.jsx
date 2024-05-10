89f8b9d330275334b67519bb0c0f700994e06389
bill7maxmaye <bilenmehalek7@gmail.com>
2024-05-10 13:48:03 +0300

    created .env and set up cloudinary

--- Client/src/pages/RegisterPage.jsx
+++ Client/src/pages/RegisterPage.jsx
@@ -1,13 +1,41 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
+import { IoClose } from "react-icons/io5";
 
 const RegisterPage = () => {
+  const [data, setData] = useState({
+    name: "",
+    email: "",
+    password: "",
+    profile_pic: "",
+  });
+  const [uploadPhoto, setUploadPhoto] = useState("");
+  //const navigate = useNavigate()
+
+  const handleOnChange = (e) => {
+    const { name, value } = e.target;
+
+    setData((preve) => {
+      return {
+        ...preve,
+        [name]: value,
+      };
+    });
+  };
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    e.stopPropagation();
+
+    //console log to see if the user input is being added to the data object on the form submission
+    //console.log("data", data);
+  };
+
   return (
     <div className="mt-20">
       <div className="bg-white w-full max-w-md  rounded overflow-hidden p-4 mx-auto">
         <h3>Welcome to Chat app!</h3>
 
-        <form className="grid gap-4 mt-5">
+        <form className="grid gap-4 mt-5" onSubmit={handleSubmit}>
           <div className="flex flex-col gap-1">
             <label htmlFor="name">Name </label>
             <input
@@ -16,8 +44,8 @@ const RegisterPage = () => {
               name="name"
               placeholder="enter your name"
               className="bg-slate-100 px-2 py-1 focus:outline-primary"
-              //value={data.name}
-              //onChange={handleOnChange}
+              value={data.name}
+              onChange={handleOnChange}
               required
             />
           </div>
@@ -30,8 +58,8 @@ const RegisterPage = () => {
               name="email"
               placeholder="enter your email"
               className="bg-slate-100 px-2 py-1 focus:outline-primary"
-              //value={data.email}
-              //onChange={handleOnChange}
+              value={data.email}
+              onChange={handleOnChange}
               required
             />
           </div>
@@ -44,8 +72,8 @@ const RegisterPage = () => {
               name="password"
               placeholder="enter your password"
               className="bg-slate-100 px-2 py-1 focus:outline-primary"
-              //value={data.password}
-              //onChange={handleOnChange}
+              value={data.password}
+              onChange={handleOnChange}
               required
             />
           </div>
@@ -53,21 +81,17 @@ const RegisterPage = () => {
           <div className="flex flex-col gap-1">
             <label htmlFor="profile_pic">
               Photo :
-              {/* <div className="h-14 bg-slate-200 flex justify-center items-center border rounded hover:border-primary cursor-pointer">
+              <div className="h-14 bg-slate-200 flex justify-center items-center border rounded hover:border-primary cursor-pointer">
                 <p className="text-sm max-w-[300px] text-ellipsis line-clamp-1">
-                  {uploadPhoto?.name
-                    ? uploadPhoto?.name
-                    : "Upload profile photo"}
+                  Upload Photo
                 </p>
-                {uploadPhoto?.name && (
-                  <button
-                    className="text-lg ml-2 hover:text-red-600"
-                    // onClick={handleClearUploadPhoto}
-                  >
-                    <IoClose />
-                  </button>
-                )}
-              </div> */}
+                <button
+                  className="text-lg ml-2 hover:text-red-600"
+                  // onClick={handleClearUploadPhoto}
+                >
+                  <IoClose />
+                </button>
+              </div>
             </label>
 
             <input
