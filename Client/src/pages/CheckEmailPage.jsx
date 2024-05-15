ad58ea5c23ed1444e1cf92d70371220e98421558
bill7maxmaye <bilenmehalek7@gmail.com>
2024-05-15 11:37:55 +0300

    created email check page

--- Client/src/pages/CheckEmailPage.jsx
+++ Client/src/pages/CheckEmailPage.jsx
@@ -2,13 +2,12 @@ import React, { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import axios from "axios";
 import toast from "react-hot-toast";
+import { PiUserCircle } from "react-icons/pi";
 
 const CheckEmailPage = () => {
   const [data, setData] = useState({
     email: "",
-    password: "",
   });
-
   const navigate = useNavigate();
 
   const handleOnChange = (e) => {
@@ -26,36 +25,38 @@ const CheckEmailPage = () => {
     e.preventDefault();
     e.stopPropagation();
 
-    //const URL = `${process.env.REACT_APP_BACKEND_URL}/api/register`;
     const URL = `${import.meta.env.VITE_BACKEND_URL}/api/email`;
+
     try {
       const response = await axios.post(URL, data);
-      console.log("response", response);
 
       toast.success(response.data.message);
 
       if (response.data.success) {
         setData({
           email: "",
-          password: "",
         });
-
-        navigate("/email");
+        navigate("/password", {
+          state: response?.data?.data,
+        });
       }
     } catch (error) {
       toast.error(error?.response?.data?.message);
     }
-    //   console.log("data", data);
   };
 
   return (
     <div className="mt-5">
       <div className="bg-white w-full max-w-md  rounded overflow-hidden p-4 mx-auto">
+        <div className="w-fit mx-auto mb-2">
+          <PiUserCircle size={80} className="text-primary" />
+        </div>
+
         <h3>Welcome to Chat app!</h3>
 
-        <form className="grid gap-4 mt-5" onSubmit={handleSubmit}>
+        <form className="grid gap-4 mt-3" onSubmit={handleSubmit}>
           <div className="flex flex-col gap-1">
-            <label htmlFor="email">Email </label>
+            <label htmlFor="email">Email :</label>
             <input
               type="email"
               id="email"
@@ -68,30 +69,13 @@ const CheckEmailPage = () => {
             />
           </div>
 
-          <div className="flex flex-col gap-1">
-            <label htmlFor="password">Password </label>
-            <input
-              type="password"
-              id="password"
-              name="password"
-              placeholder="enter your password"
-              className="bg-slate-100 px-2 py-1 focus:outline-primary"
-              value={data.password}
-              onChange={handleOnChange}
-              required
-            />
-          </div>
-
-          <button
-            type="submit"
-            className="bg-primary text-lg  px-4 py-1 hover:bg-secondary rounded mt-2 font-bold text-white leading-relaxed tracking-wide"
-          >
-            Login
+          <button className="bg-primary text-lg  px-4 py-1 hover:bg-secondary rounded mt-2 font-bold text-white leading-relaxed tracking-wide">
+            Let's Go
           </button>
         </form>
 
         <p className="my-3 text-center">
-          No Account Register ?{" "}
+          New User ?{" "}
           <Link to={"/register"} className="hover:text-primary font-semibold">
             Register
           </Link>
