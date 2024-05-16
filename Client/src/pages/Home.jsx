2b645286215f568bba2a8b29baeb2bf39e7db1a8
bill7maxmaye <bilenmehalek7@gmail.com>
2024-05-16 23:54:16 +0300

    added socket io connection

--- Client/src/pages/Home.jsx
+++ Client/src/pages/Home.jsx
@@ -1,25 +1,33 @@
+import axios from "axios";
 import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { Outlet, useNavigate } from "react-router-dom";
+import { Outlet, useLocation, useNavigate } from "react-router-dom";
+import {
+  logout,
+  setOnlineUser,
+  setSocketConnection,
+  setUser,
+} from "../redux/userSlice";
 import Sidebar from "../components/Sidebar";
-import axios from "axios";
+import logo from "../assets/logo.png";
+import io from "socket.io-client";
 
 const Home = () => {
   const user = useSelector((state) => state.user);
   const dispatch = useDispatch();
   const navigate = useNavigate();
+  const location = useLocation();
 
   console.log("user", user);
   const fetchUserDetails = async () => {
     try {
-      const URL = `${import.meta.env.VITE_BACKEND_URL}/api/user-details`;
-
+      const URL = `${process.env.REACT_APP_BACKEND_URL}/api/user-details`;
       const response = await axios({
         url: URL,
         withCredentials: true,
       });
 
-      dispatch(setUser(response?.data?.data));
+      dispatch(setUser(response.data.data));
 
       if (response.data.data.logout) {
         dispatch(logout());
@@ -35,14 +43,50 @@ const Home = () => {
     fetchUserDetails();
   }, []);
 
+  /***socket connection */
+  useEffect(() => {
+    const socketConnection = io(`${import.meta.env.VITE_BACKEND_URL}`, {
+      auth: {
+        token: localStorage.getItem("token"),
+      },
+    });
+
+    socketConnection.on("onlineUser", (data) => {
+      console.log(data);
+      dispatch(setOnlineUser(data));
+    });
+
+    dispatch(setSocketConnection(socketConnection));
+
+    return () => {
+      socketConnection.disconnect();
+    };
+  }, []);
+
+  const basePath = location.pathname === "/";
   return (
     <div className="grid lg:grid-cols-[300px,1fr] h-screen max-h-screen">
-      <section className={`bg-white  lg:block`}>
+      <section className={`bg-white ${!basePath && "hidden"} lg:block`}>
         <Sidebar />
       </section>
-      <section>
+
+      {/**message component**/}
+      <section className={`${basePath && "hidden"}`}>
         <Outlet />
       </section>
+
+      <div
+        className={`justify-center items-center flex-col gap-2 hidden ${
+          !basePath ? "hidden" : "lg:flex"
+        }`}
+      >
+        <div>
+          <img src={logo} width={250} alt="logo" />
+        </div>
+        <p className="text-lg mt-2 text-slate-500">
+          Select user to send message
+        </p>
+      </div>
     </div>
   );
 };
