5b575140bb9349bbbb220e6e64a13d409d7c73c2
bill7maxmaye <bilenmehalek7@gmail.com>
2024-05-16 23:30:53 +0300

    created home page

--- Client/src/pages/Home.jsx
+++ Client/src/pages/Home.jsx
@@ -1,10 +1,45 @@
-import React from "react";
-import { Outlet } from "react-router-dom";
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { Outlet, useNavigate } from "react-router-dom";
+import Sidebar from "../components/Sidebar";
+import axios from "axios";
 
 const Home = () => {
+  const user = useSelector((state) => state.user);
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
+  console.log("user", user);
+  const fetchUserDetails = async () => {
+    try {
+      const URL = `${import.meta.env.VITE_BACKEND_URL}/api/user-details`;
+
+      const response = await axios({
+        url: URL,
+        withCredentials: true,
+      });
+
+      dispatch(setUser(response?.data?.data));
+
+      if (response.data.data.logout) {
+        dispatch(logout());
+        navigate("/email");
+      }
+      console.log("current user Details", response);
+    } catch (error) {
+      console.log("error", error);
+    }
+  };
+
+  useEffect(() => {
+    fetchUserDetails();
+  }, []);
+
   return (
-    <div>
-      Home
+    <div className="grid lg:grid-cols-[300px,1fr] h-screen max-h-screen">
+      <section className={`bg-white  lg:block`}>
+        <Sidebar />
+      </section>
       <section>
         <Outlet />
       </section>
